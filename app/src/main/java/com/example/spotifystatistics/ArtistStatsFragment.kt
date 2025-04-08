792803172de5f73d3db6d933dbd9098286cf9af2
kazaz03 <akazazovic1@etf.unsa.ba>
2025-04-08 20:54:15 +0200

    artist info and stats fragment added, top nav menu

--- app/src/main/java/com/example/spotifystatistics/ArtistStatsFragment.kt
+++ app/src/main/java/com/example/spotifystatistics/ArtistStatsFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ArtistInfoFragment.newInstance] factory method to
+ * Use the [ArtistStatsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ArtistInfoFragment : Fragment() {
+class ArtistStatsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class ArtistInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_artist_info, container, false)
+        return inflater.inflate(R.layout.fragment_artist_stats, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class ArtistInfoFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ArtistInfoFragment.
+         * @return A new instance of fragment ArtistStatsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ArtistInfoFragment().apply {
+            ArtistStatsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/spotifystatistics/ProfileFragment.kt
+++ app/src/main/java/com/example/spotifystatistics/ProfileFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -69,7 +68,7 @@ class ProfileFragment : Fragment() {
     private fun openArtistInfoFragment(name: String){
         //parent fragment manager because im already in a fragment that has its fm
         val fragmentManager=parentFragmentManager
-        fragmentManager.beginTransaction().replace(R.id.fragmentPart,ArtistInfoFragment())
+        fragmentManager.beginTransaction().replace(R.id.fragmentPart,ArtistFragment())
             .setReorderingAllowed(true).addToBackStack(null).commit()
     }
     companion object {
